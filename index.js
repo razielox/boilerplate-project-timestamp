48589797f859a06efd68737e9efffd3a9240740f
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-12 13:00:14 -0500

    port update

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ app.get('/api/:date', (request, response) => {
 
 
 // listen for requests :)
-var listener = app.listen('3000', function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
